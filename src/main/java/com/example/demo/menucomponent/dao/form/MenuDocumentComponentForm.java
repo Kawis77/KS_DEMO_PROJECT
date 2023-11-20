cee44bbdf223fdefd522d05fda90851bd730bd67
Kawis77 <krzysztofsiwak77@gmail.com>
2023-11-20 21:23:12 +0100

    changes in move document methode (send forms)

--- src/main/java/com/example/demo/menucomponent/dao/form/MenuDocumentComponentForm.java
+++ src/main/java/com/example/demo/menucomponent/dao/form/MenuDocumentComponentForm.java
@@ -1,11 +1,34 @@
 package com.example.demo.menucomponent.dao.form;
 
+import com.example.demo.document.dao.entity.DocumentEntity;
+
+import java.util.List;
+
 public class MenuDocumentComponentForm {
 
+    Long id;
     private String name;
 
     private String description;
 
+    private List<DocumentEntity> documents;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<DocumentEntity> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<DocumentEntity> documents) {
+        this.documents = documents;
+    }
 
     public String getName() {
         return name;
--- src/main/java/com/example/demo/menucomponent/service/MenuDocumentComponentService.java
+++ src/main/java/com/example/demo/menucomponent/service/MenuDocumentComponentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.menucomponent.service;
 
 import com.example.demo.menucomponent.dao.entity.MenuDocumentComponentEntity;
+import com.example.demo.menucomponent.dao.form.MenuDocumentComponentForm;
 import com.example.demo.menucomponent.dao.repository.MenuDocumentComponentRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -72,6 +74,15 @@ public class MenuDocumentComponentService {
             }
         }
         return null;
+    }
+
+
+    public MenuDocumentComponentForm mappedEntityToForm(MenuDocumentComponentEntity menuDocumentComponent){
+        MenuDocumentComponentForm componentForm = new MenuDocumentComponentForm();
+        componentForm.setId(menuDocumentComponent.getId());
+        componentForm.setName(menuDocumentComponent.getName());
+        componentForm.setDescription(menuDocumentComponent.getDescription());
+        return componentForm;
     }
 
 }
